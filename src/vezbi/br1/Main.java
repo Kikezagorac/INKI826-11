e9d29fa48bae79dc4228d2c1d207441823e3bf76
kristinazagorac <97107325+kristinazagorac@users.noreply.github.com>
2023-01-04 19:33:25 +0100

    Vezbi broj 11

---
+++ src/vezbi/br1/Main.java
@@ -0,0 +1,13 @@
+package vezbi.br1;
+
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		Datoteka d = new Datoteka ("file.txt");
+		d.zapishi("Tekstot");
+		d.prochitaj();
+	}
+}
+
---
+++ src/vezbi/br2/Main.java
@@ -0,0 +1,19 @@
+package vezbi.br2;
+
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int x;
+		String s="";
+		try {
+			System.out.println("Vnesete tekst:");
+			while((x = System.in.read()) != 13)
+				s+=(char)x;
+				System.out.println(s);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
